44b86f2cd7a40f41e8f38f463f86b146ed612070
David Lee <developer@leedium.com>
2019-03-17 09:43:36 -0400

    test successful transefer of all collection images to two environments, updated readme, and prgram instructions

--- app/app.js
+++ app/app.js
@@ -25,11 +25,17 @@ const fs = require('fs-extra');
 const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminGifsicle = require('imagemin-gifsicle');
 
 const zipFileName = "images.zip"
 const resObj = require('./restObj');
 const packageJSON = require('../package');
 const extensionsArray = ['.jpg', '.png', '.gif', 'svg', '.tiff', '.tif'];
+const uploadFileTypeHash = {
+    "collections": "collectionImage",
+    "products": "productImage",
+    "general": "general"
+};
 const uploadTypes = {
     productImage: "productImage",
     general: "general",
@@ -37,19 +43,24 @@ const uploadTypes = {
 };
 const occTokenGenerator = require("./occ-token-generator");
 const distFolder = 'dist';
+const optimzedFolder = 'optimized';
 const apiFilesEndpoint = '/ccadmin/v1/files';
 const list = val => val.split(',');
 
-Promise.each = function(arr, func) {
+Promise.each = function (arr, func) {
     // if arr is not an array reject the process
-    if(!Array.isArray(arr)){return Promise.reject(new Error(`The first argument passed must be of type Array`))}
+    if (!Array.isArray(arr)) {
+        return Promise.reject(new Error(`The first argument passed must be of type Array`))
+    }
 
     // if arr is empty return a "completed promise"
-    if(arr.length === 0){return Promise.resolve()}
+    if (arr.length === 0) {
+        return Promise.resolve()
+    }
 
     // Start the flow with a resolved Promise and return the "then" with function execution of the next Promise
-    return arr.reduce(function(a,b){
-        return a.then(()=> func(b));
+    return arr.reduce(function (a, b) {
+        return a.then(() => func(b));
     }, Promise.resolve())
 }
 
@@ -60,20 +71,15 @@ Promise.each = function(arr, func) {
  */
 const start = async (program) => {
     try {
-        const filehound = FileHound.create();
-        const zipFileName = 'images.zip';
-        const zip = new AdmZip();
-        const uploadType = program.uploadtype || uploadTypes.general;
-        let images;
-        let imgPath = program.imagepath;
 
         // get the list of files from the source server
+        console.log(program.sourceserver)
         try {
             var {items} = await resObj.apiCall(
-                program.servers[0],
-                program.keys[0],
+                program.sourceserver,
+                program.sourceserverkey,
                 'GET',
-                `${apiFilesEndpoint}?folder=/${program.uploadtype}&fields=url`, {});
+                `${apiFilesEndpoint}?folder=/${program.imagetype}&fields=url`, {});
             // loop through and save all these files to the file system
             // Promise.all(items.map(({url}_=> {
             //     download(url, 'dist').then(data => {
@@ -84,32 +90,29 @@ const start = async (program) => {
             // }));
 
 
-
             // Promise.all(items.map(filePath =>
             //     // console.log('==>',filePath.url);
             //     download(filePath.url, 'dist', {
             //         filename:new URL(filePath.url).pathname.split('/').pop()
             //     }).then(data => {})))
             //         // console.log(new URL(filePath.url).pathname.split('/').pop())
-                   // const file = new URL(filePath.url).pathname.split('/').pop();
-                   // console.log('d:',file);
-                   //  fs.writeFileSync(file, data);
+            // const file = new URL(filePath.url).pathname.split('/').pop();
+            // console.log('d:',file);
+            //  fs.writeFileSync(file, data);
 
-                //     .then(() => {
-                //     // fs.writeFileSync(new URL(filePath.url).pathname.split('/').pop(), data);
-                //     console.log('done');
-                // });
+            //     .then(() => {
+            //     // fs.writeFileSync(new URL(filePath.url).pathname.split('/').pop(), data);
+            //     console.log('done');
+            // });
 
-                // download(filePath.url,'dist').pipe(fs.createWriteStream(`dist/${new URL(filePath.url).pathname.split('/').pop()}`))
+            // download(filePath.url,'dist').pipe(fs.createWriteStream(`dist/${new URL(filePath.url).pathname.split('/').pop()}`))
 
 
             await downloadImages(items);
-            console.log('images download complete');
+            await uploadImages();
 
 
-
-
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
 
@@ -151,7 +154,7 @@ const start = async (program) => {
 
         // images.forEach(file => {
         //     console.log(file)
-            // zip.addLocalFile(file);
+        // zip.addLocalFile(file);
         // });
 
         // zip.toBuffer(async (successBuffer) => {
@@ -188,7 +191,7 @@ const start = async (program) => {
         //     (err) => {
         //         console.log(err.message);
         //     });
-    }catch (err) {
+    } catch (err) {
         console.log(`Error: ${err.message}`)
     }
 };
@@ -201,30 +204,108 @@ const start = async (program) => {
 const downloadImages = (images) =>
     new Promise((resolve) => {
         fs.ensureDir(distFolder);
-        fs.emptyDir(distFolder).then(async () => {
-
-            // download all the images from the source
-            const token = await occTokenGenerator.generateToken(program.servers[0], program.keys[0]);
-            let counter = 0;
-            images.map(filePath =>{
-                const file = new URL(filePath.url).pathname.split('/').pop();
-                got.stream(filePath.url,{
-                    headers:{
-                        'Authorization': `Bearer ${token}`
-                    }
-                })
-                    .pipe(fs.createWriteStream(`dist/${file}`))
-                    .on('finish', () => {
-                        counter += 1;
-                        console.log(file);
-                        if(counter === images.length){
-                            resolve();
+        fs.ensureDir(optimzedFolder);
+        Promise.all([
+            fs.emptyDir(distFolder),
+            fs.emptyDir(optimzedFolder)
+        ])
+            .then(async () => {
+                // download all the images from the source
+                const token = await occTokenGenerator.generateToken(program.sourceserver, program.sourceserverkey);
+                let counter = 0;
+                images.map(filePath => {
+                    const file = new URL(filePath.url).pathname.split('/').pop();
+                    got.stream(filePath.url, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
                         }
                     })
+                        .pipe(fs.createWriteStream(`dist/${file}`))
+                        .on('finish', () => {
+                            counter += 1;
+                            console.log(file);
+                            if (counter === images.length) {
+                                resolve();
+                            }
+                        })
+                });
+            })
+    });
+
+/**
+ *
+ */
+const uploadImages = () =>
+    new Promise(async () => {
+        const filehound = FileHound.create();
+        const zipFileName = 'images.zip';
+        const zip = new AdmZip();
+        const uploadType = program.imagetype || uploadTypes.general;
+        let isOptimized = false;
+        if (typeof program.optimize !== 'undefined') {
+            console.log('optimizing images');
+            isOptimized = true;
+            if (program.optimize < 1 && program.optimize > 0) {
+                await imagemin([`./${distFolder}/**/*.{jpg,png,gif}`], `./${optimzedFolder}`, {
+                    plugins: [
+                        imageminMozjpeg({quality: program.optimize * 100}),
+                        imageminPngquant({
+                            quality: [program.optimize, program.optimize]
+                        }),
+                        imageminGifsicle()
+                    ]
+                });
+                console.log('optimization complete')
+            } else {
+                throw new Error("quality must be between 0 and 1")
+            }
+        }
+        const images = filehound
+            .path(isOptimized ? optimzedFolder : distFolder)
+            .ext(extensionsArray)
+            .findSync();
+
+        images.forEach(file => {
+            console.log(file)
+            zip.addLocalFile(file);
+        });
+
+        zip.toBuffer(async (successBuffer) => {
+                try {
+                    const buffer64 = Buffer.from(successBuffer).toString('base64')
+                    const tokens = await Promise.all(program.targetservers.map((item, i) => {
+                        return resObj.apiCall(
+                            item,
+                            program.targetserverkeys[i],
+                            'PUT',
+                            apiFilesEndpoint, {
+                                filename: zipFileName,
+                                segments: 1,
+                                uploadtype: uploadFileTypeHash[uploadType]
+                            })
+                    }));
+
+                    await Promise.all(program.targetservers.map((item, i) => {
+                        resObj.apiCall(
+                            item,
+                            program.targetserverkeys[i],
+                            'POST',
+                            `${apiFilesEndpoint}/${tokens[i].token}`, {
+                                file: buffer64,
+                                filename: zipFileName,
+                                index: 0
+                            });
+                    }));
+
+
+                    console.log(`Transfer to ${program.targetservers.length} environments complete.`)
+                } catch (err) {
+                    console.log(err.message);
+                }
+            },
+            (err) => {
+                console.log(err.message);
             });
-
-
-        })
     });
 
 // main entry function
@@ -240,21 +321,30 @@ exports.main = function (argv) {
         )
         //
         .option(
-            "-s, --servers <items>",
-            "Comma delimited string with the server definitions ie: server FROM,server TO",
-            list
+            "-s, --sourceserver <sourceserver>",
+            "Source server where images will be downloaded from"
         )
+        //
         .option(
-            "-t, --keys <items>",
-            "Occ Admin api key for server ie: [server key source],[server key target]",
-            list
+            "-s, --sourceserverkey <sourceserverkey>",
+            "Source server key"
         )
+        //
+        .option(
+            "-s, --targetservers <items>",
+            "Comma delimited string with the server definitions ie: server1,server2,serverN",
+            list
+        ).option(
+        "-t, --targetserverkeys <items>",
+        "Occ Admin api key for server ie: [server key source],[server key target]",
+        list
+    )
         .option(
             "-d, --imagepath <imagepath>",
             "path to folder to generate the image zipfile"
         )
         .option(
-            "-u, --uploadtype <uploadtype>",
+            "-u, --imagetype <uploadtype>",
             "Upload Type < general, collections, product >"
         )
         .option(
